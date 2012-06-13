7c2b3b9aa52b571e8a04d1f3c6b6276c35a72a16
Carlos Caballero <cijkb.j@gmail.com>
2012-06-13 04:39:41 -0400

    [TEMPLATE] Finished the template

--- templates/carlos.php
+++ templates/carlos.php
@@ -1,37 +1,21 @@
-<!DOCTYPE html PUBLIC
-    "-//W3C//DTD XHTML 1.0 Strict//EN"
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
-        <title>~ NonChalant (α1) ~</title>
+        <title>/bin/nch</title>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <link href="/nonchalant/css/carlos.css" media="screen" rel="stylesheet" type="text/css" />
+        <link href="docs/scesi/nonchalant.png" rel="icon" />
+        <link href="css/carlos.css" media="screen" rel="stylesheet" type="text/css" />
     </head>
     <body>
-        <div id="main">
-            <pre>asdfasdf adsfasdflkj asdfa asdfasdf adsfasdflkj asdfa
-asdfasdf adsfasdflkj asdfa asdfasdf adsf
-asdflkj asdfa asdfasdf adsfasdflkj asdfa asdfasdf adsfasdflkj asdfa
-asdfasdf adsfasdflkj asdfaasdfa sdf adsfasdflkj asdfa
-asdfasdf adsfasdflkj asdfaasdf asdf adsfasdflkj asdfa
-asdfasdf adsfasdflkj asdfaa sdfasdf adsfasdflkj asdfa
-asdfasdf adsfasdflkj asdfaa dfasdf adsfasdflkj asdfa
-asdfasdf adsfasdflkj asdfa</pre>
-            <div id="form">
-                <form method="post" action="">
-                    $ <input name="comando" type="text" />
-                </form>
+        <div id="header"></div>
+        <div id="wrapper">
+            <div id="main">
+                <pre><?php echo $this->output ?></pre>
+                <form action="" method="post"># <input id="command" name="comando" type="text" /></form>
             </div>
+            <div id="footer"><a href="http://scesi.fcyt.umss.edu.bo/" target="_BLANK">SCESI</a><a href="http://www.memi.umss.edu.bo/" target="_BLANK">MEMI</a><a href="https://github.com/ccaballero/nonchalant">Código fuente</a></div>
         </div>
-        <div class="footer">
-            <a href="http://scesi.fcyt.umss.edu.bo/" target="_BLANK">SCESI</a>
-            <a class="border" href="http://www.memi.umss.edu.bo/" target="_BLANK">MEMI</a>
-            <a class="border" href="/static/terms">Términos de uso</a>
-            <a class="border" href="/static/privacy">Privacidad</a>
-
-            <a class="border" href="/static/development">Desarrollo</a>
-            <a class="border" href="https://github.com/ccaballero/babel">Código fuente</a>
-            <a class="border" href="/help">Ayuda</a>
-       </div>
+        <script type="text/javascript">window.onload=function(){document.getElementById("command").focus();}</script>
     </body>
 </html>
--- templates/default.php
+++ templates/default.php
@@ -4,7 +4,6 @@
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     </head>
     <body>
-        <h1></h1>
         <pre><?php echo $this->output ?></pre>
         <form action="" method="post">
             <input name="comando" type="text" />
