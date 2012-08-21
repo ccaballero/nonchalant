b24bd4445bfb708a450b2afb12a90c6afd27e9f2
baudi <klb.imfo642@gmail.com>
2012-08-21 12:59:43 -0400

    [TEMPLATE] my template

---
+++ templates/baudi.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>Nonchalant2</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    </head>
+    <body style="background: yellow">
+        <pre><?php echo implode('', $this->outputs) ?></pre>
+        <form action="" method="post">
+            <input name="comando" type="text" />
+            <input type="submit" />
+        </form>
+    </body>
+</html>
+
