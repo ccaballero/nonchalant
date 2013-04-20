4506b0bb11993ee7e382264dd6d40a28d82edcee
Franz Lopez <franzelunico@gmail.com>
2013-04-20 15:05:18 -0400

    working on vars for shell

--- new/bin/echo.php
+++ new/bin/echo.php
@@ -3,9 +3,16 @@
 class _Echo implements Command
 {
     public function execute($parameter) {
+        global $VARS;
+        
         if (!empty($parameter[1])) {
             unset($parameter[0]);
-            $result = implode(' ', $parameter);
+            $result = trim(implode(' ', $parameter));
+            
+            if (preg_match("/\\$[a-zA-Z_][a-zA-Z0-9_]*/", $result)) {
+                $var = substr($result, 1);
+                $result = $VARS[$var];
+            }
         }
         return $result;
     }
--- new/index.php
+++ new/index.php
@@ -1,31 +1,40 @@
 <?php
-
 global $HISTORIAL;
 $HISTORIAL = $_POST['historial'];
+
+global $VARS;
+$VARS = unserialize($_POST['vars']);
 
 $result = '';
 $final_result = '';
 
 if (isset($_POST['command'])) {
     $commands = preg_split('/(?<!\\\\);/', $_POST['command']);
+    include 'bin/Command.php';
 
     foreach ($commands as $command) {
         $command = ltrim($command);
-        $parameter = explode(' ', $command);
-
-        include 'bin/Command.php';
-        if (file_exists('bin/' . $parameter[0] . '.php')) {
-            include 'bin/' . $parameter[0] . '.php';
-        }
-
-        $_class = ucfirst($parameter[0]);
-        $class = class_exists('_' . $_class) ? '_' . $_class :
-                 (class_exists($_class) ? $_class : null);
-        if ($class <> null) {
-            $object = new $class();
-            $result = $object->execute($parameter);
+        if (preg_match("/[a-zA-Z_][a-zA-Z0-9_]*=.*/", $command)) {
+            list($a, $b) = explode('=', $command);
+            
+            $VARS[$a] = $b;
+            
         } else {
-            $result = 'nch: ' . $parameter[0] . ': command not found';
+            $parameter = explode(' ', $command);
+            
+            if (file_exists('bin/' . $parameter[0] . '.php')) {
+                include_once 'bin/' . $parameter[0] . '.php';
+            }
+
+            $_class = ucfirst($parameter[0]);
+            $class = class_exists('_' . $_class) ? '_' . $_class :
+                    (class_exists($_class) ? $_class : null);
+            if ($class <> null) {
+                $object = new $class();
+                $result = $object->execute($parameter);
+            } else {
+                $result = 'nch: ' . $parameter[0] . ': command not found';
+            }
         }
 
         $final_result .= $result . PHP_EOL;
@@ -45,6 +54,7 @@ if (isset($_POST['command'])) {
         <pre><?php echo $HISTORIAL ?></pre>
         <form action="" method="post">
             <input type="hidden" name="historial" value="<?php echo $HISTORIAL ?>" />
+            <input type="hidden" name="vars" value="<?php echo htmlentities(serialize($VARS)) ?>" />
             <input name="command" type="text" />
             <input type="submit" value="execute"/>
         </form>
