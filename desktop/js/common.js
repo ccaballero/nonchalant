13fba60d54975badefee92a5d14d16212e74f8d8
Carlos Eduardo Caballero Burgoa <cijkb.j@gmail.com>
2013-05-23 08:39:28 -0400

    [DESKTOP] Working on the window manager for nch

---
+++ desktop/js/common.js
@@ -0,0 +1,25 @@
+/* common function of extract information and utilities for resizing */
+
+var width_navigator=function(){
+    if(window.innerWidth){
+        return window.innerWidth
+    }else{
+        return document.documentElement.clientWidth
+    }}
+var height_navigator=function(){
+    if(window.innerWidth){
+        return window.innerWidth
+    }else{
+        return document.documentElement.clientWidth
+    }}
+
+var resize_taskbar=function(){
+    var res_width=$('#panel div.home').width()+1
+        +$('#panel div.tags').width()+1
+        +$('#panel div.layout').width()+1+2
+    var width=width_navigator()-res_width
+    var count_tasks=$('#panel div.tasks ul li.task').length
+    $('#panel div.tasks').css('width',width+'px')
+    $('#panel div.tasks ul li.task').each(function(){
+        $(this).css('width',width/count_tasks)
+    })}
